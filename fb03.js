f3979408116bc1ed6106713a00ba38a711011b73
rover egy <37258594+roveregy@users.noreply.github.com>
2019-02-12 04:03:54 +0300

    Create fb03.js

---
+++ fb03.js
@@ -0,0 +1,11 @@
+//<![CDATA[
+
+  setTimeout(function () {
+    $(".pre-loader").fadeOut("slow");
+  }, 6000);
+
+  $(window).on("load", function () {
+    $(".pre-loader").fadeOut("slow");
+  });
+
+//]]>
